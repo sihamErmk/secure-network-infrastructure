cd379d8f09949ce1b7c1a90c4bd998d2bb607855
sihamErmk <selkouaze@gmail.com>
2026-01-02 17:14:36 +0100

    steup dmz

--- setup_dmz.sh
+++ setup_dmz.sh
@@ -1,28 +1,38 @@
-# dmz_setup.sh
-# Certificat SSL
-mkdir -p /etc/ssl/private
+#!/bin/bash
+# Configuration Apache + SSL sur h_dmz
+
+# 1. Nettoyage et Dossiers
+service apache2 stop
+mkdir -p /etc/ssl/private /etc/ssl/certs
+
+# 2. Génération Certificat SSL
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout /etc/ssl/private/apache.key -out /etc/ssl/certs/apache.crt \
     -subj "/C=FR/O=LSI3/CN=10.0.1.2"
 
-# Redirection 80 -> 443
+# 3. Correction du fichier ports (TRÈS IMPORTANT)
+printf "Listen 80\nListen 443\n" > /etc/apache2/ports.conf
+
+# 4. Config VirtualHost HTTPS
+cat <<EOF > /etc/apache2/sites-available/default-ssl.conf
+<VirtualHost *:443>
+    DocumentRoot /var/www/html
+    SSLEngine on
+    SSLCertificateFile /etc/ssl/certs/apache.crt
+    SSLCertificateKeyFile /etc/ssl/private/apache.key
+</VirtualHost>
+EOF
+
+# 5. Config Redirection HTTP -> HTTPS
 cat <<EOF > /etc/apache2/sites-available/000-default.conf
 <VirtualHost *:80>
     Redirect permanent / https://10.0.1.2/
 </VirtualHost>
 EOF
 
-# HTTPS Config
-cat <<EOF > /etc/apache2/sites-available/default-ssl.conf
-<VirtualHost *:443>
-    SSLEngine on
-    SSLCertificateFile /etc/ssl/certs/apache.crt
-    SSLCertificateKeyFile /etc/ssl/private/apache.key
-    DocumentRoot /var/www/html
-</VirtualHost>
-EOF
+# 6. Activation modules et site
+a2enmod ssl rewrite > /dev/null
+a2ensite default-ssl 000-default > /dev/null
 
-a2enmod ssl > /dev/null
-a2ensite default-ssl > /dev/null
-echo "Listen 80\nListen 443" > /etc/apache2/ports.conf
+# 7. Redémarrage
 service apache2 restart
