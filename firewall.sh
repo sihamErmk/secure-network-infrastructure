52b72a5ed551770df7c7eea404ac81a8da86d33c
sihamErmk <selkouaze@gmail.com>
2026-01-02 15:34:18 +0100

    Segmentation & Pare-feu

--- firewall.sh
+++ firewall.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-# Reset
+# Nettoyage
 iptables -F
 iptables -X
-iptables -t nat -F
-
-# Politique par défaut : DROP TOTAL
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
-iptables -P OUTPUT ACCEPT
+iptables -P FORWARD DROP  # Politique ZERO TRUST
 
 # Autoriser le trafic établi (Stateful)
 iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-# 1. WAN -> DMZ (HTTPS uniquement)
-iptables -A FORWARD -i fw1-eth0 -o fw1-eth1 -p tcp --dport 443 -d 10.0.1.2 -j ACCEPT
+# Règle DMZ : WAN -> DMZ (Port 443 seulement)
+iptables -A FORWARD -i fw1-eth0 -o fw1-eth1 -p tcp --dport 443 -d 10.0.1.10 -j ACCEPT
 
-# 2. VPN -> ADMIN (Accès distant sécurisé)
-iptables -A FORWARD -i fw1-eth3 -o fw1-eth4 -p tcp --dport 22 -j ACCEPT
+# Règle ADMIN : VPN -> LAN/DMZ (SSH seulement)
+iptables -A FORWARD -i fw1-eth3 -o fw1-eth2 -p tcp --dport 22 -j ACCEPT
 
-# 3. Journalisation
-iptables -A FORWARD -j LOG --log-prefix "ZERO-TRUST-DROP: "
+# Journalisation (Exigence 5.1)
+iptables -A FORWARD -j LOG --log-prefix "FIREWALL-DROP: "
