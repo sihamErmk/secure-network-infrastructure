c4ac547eb69f16a36eb9f1c24816a3f72b18c77f
sihamErmk <selkouaze@gmail.com>
2026-01-02 17:05:16 +0100

    L'Infrastructure de base

--- topologie.py
+++ topologie.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+# topo.py
 from mininet.net import Mininet
 from mininet.node import Node, OVSSwitch
 from mininet.cli import CLI
@@ -8,49 +8,43 @@ class LinuxRouter(Node):
     def config(self, **params):
         super(LinuxRouter, self).config(**params)
         self.cmd('sysctl -w net.ipv4.ip_forward=1')
-    def terminate(self):
-        self.cmd('sysctl -w net.ipv4.ip_forward=0')
-        super(LinuxRouter, self).terminate()
 
 def run_topo():
     net = Mininet(topo=None, build=False, controller=None)
 
-    info('*** Création du Pare-feu (fw)\n')
-    fw = net.addHost('fw', cls=LinuxRouter, ip='10.0.0.1/24')
+    info('*** Création des Routeurs (Cluster HA)\n')
+    fw1 = net.addHost('fw1', cls=LinuxRouter, ip='10.0.0.1/24')
+    fw2 = net.addHost('fw2', cls=LinuxRouter, ip='10.0.0.254/24') # Backup
 
-    info('*** Création des hôtes et zones\n')
+    info('*** Création des Hôtes par zone\n')
     h_wan = net.addHost('h_wan', ip='10.0.0.2/24', defaultRoute='via 10.0.0.1')
     h_dmz = net.addHost('h_dmz', ip='10.0.1.2/24', defaultRoute='via 10.0.1.1')
     h_lan = net.addHost('h_lan', ip='10.0.2.2/24', defaultRoute='via 10.0.2.1')
     h_vpn = net.addHost('h_vpn', ip='10.0.3.2/24', defaultRoute='via 10.0.3.1')
     h_adm = net.addHost('h_adm', ip='10.0.4.2/24', defaultRoute='via 10.0.4.1')
 
-    # Switches
-    s_wan, s_dmz, s_lan, s_vpn, s_adm = [net.addSwitch(s, cls=OVSSwitch, failMode='standalone') for s in ['s1','s2','s3','s4','s5']]
+    # Switches pour chaque zone
+    switches = [net.addSwitch(f's{i}', cls=OVSSwitch, failMode='standalone') for i in range(1, 6)]
 
-    # Liens
-    net.addLink(fw, s_wan, intfName1='fw-eth0')
-    net.addLink(fw, s_dmz, intfName1='fw-eth1')
-    net.addLink(fw, s_lan, intfName1='fw-eth2')
-    net.addLink(fw, s_vpn, intfName1='fw-eth3')
-    net.addLink(fw, s_adm, intfName1='fw-eth4')
-    
-    net.addLink(h_wan, s_wan)
-    net.addLink(h_dmz, s_dmz)
-    net.addLink(h_lan, s_lan)
-    net.addLink(h_vpn, s_vpn)
-    net.addLink(h_adm, s_adm)
+    # Connexions FW1 aux 5 switches
+    for i, s in enumerate(switches):
+        net.addLink(fw1, s, intfName1=f'fw1-eth{i}')
+        net.addLink(fw2, s, intfName1=f'fw2-eth{i}')
+
+    # Connexions Hôtes
+    net.addLink(h_wan, switches[0])
+    net.addLink(h_dmz, switches[1])
+    net.addLink(h_lan, switches[2])
+    net.addLink(h_vpn, switches[3])
+    net.addLink(h_adm, switches[4])
 
     net.build()
     net.start()
-
-    # IPs supplémentaires sur le routeur
-    fw.cmd('ip addr add 10.0.1.1/24 dev fw-eth1')
-    fw.cmd('ip addr add 10.0.2.1/24 dev fw-eth2')
-    fw.cmd('ip addr add 10.0.3.1/24 dev fw-eth3')
-    fw.cmd('ip addr add 10.0.4.1/24 dev fw-eth4')
-
-    info('*** Réseau prêt. Utilisez "fw bash firewall.sh" et "h_dmz bash setup_dmz.sh" ***\n')
+    
+    # Adresses IP des passerelles sur FW1
+    for i in range(1, 5):
+        fw1.cmd(f'ip addr add 10.0.{i}.1/24 dev fw1-eth{i}')
+    
     CLI(net)
     net.stop()
 
